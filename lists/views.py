70fe950cf4b63973b6bd17cc683ef72d72694ee8
AlexGreenn40 <zelalex40@gmail.com>
2023-05-07 15:41:14 +0300

    Every list - his URLs

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -17,5 +17,6 @@ def view_list(request):
 
 def new_list(request):
     """новый список"""
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/one-list-in-world/')
