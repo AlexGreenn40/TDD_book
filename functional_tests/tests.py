837f831d00299840f73da350a06c218b6d54465d
AlexGreenn40 <zelalex40@gmail.com>
2023-05-07 13:27:12 +0300

    Create new test for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -35,8 +35,8 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5) 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        """тест: можно начать список и получить его позже"""
+    def test_can_start_a_list_for_one_user(self):
+        """тест: можно начать список для одного пользователя"""
         # Эдит слышала про крутое новое онлайн-приложение со списком
         # неотложных дел. Она решает оценить его домашнюю страницу
         self.browser.get(self.live_server_url)
@@ -58,10 +58,10 @@ class NewVisitorTest(LiveServerTestCase):
         # (ее хобби – вязание рыболовных мушек)
         inputbox.send_keys('Купить павлиньи перья')
 
-        # Когда она нажимает enter, страница обновляется, и теперь страница
-        # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        # Когда она нажимает enter, страница обновляется, 
         inputbox.send_keys(Keys.ENTER)
-                
+        
+        # Теперь страница содержит "1: Купить павлиньи перья" в качестве элемента списка
         self.wait_for_row_in_list_table('1: Купить павлиньи перья')
         
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
@@ -74,15 +74,65 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Купить павлиньи перья')
         self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
         
+        # Удовлетворенная, она снова ложится спать
+
+    def test_multiple_users_can_start_list_at_different_urls(self):
+        """тест: многочисленные пользователи могут начать списки по разным url"""
+        # Эдит начинает новый список
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+
+        # Она замечает, что ее список имеет уникальный URL-адрес
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists.+')
+
+
+        # Теперь новый пользователь, Фрэнсис, приходит на сайт.
+
+        ## Мы используем новый сеанс браузера, тем самым обеспечивая, чтобы никакая
+        ## информация от Эдит не прошла через данные cookie и пр.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Фрэнсис посещает домашнюю страницу. Нет никаких признаков списка Эдит
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать мушку', page_text)
+
+        # Фрэнсис начинает новый список, вводя новый элемент. 
+        # Он менее интересен, чем список Эдит...
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+
+        # Фрэнсис получает уникальный URL-адрес
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Опять-таки, нет ни следа от списка Эдит
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Купить молоко', page_text)
+
+
+        # Удовлетворенные, они оба ложатся спать
+
+
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
         # выводится небольшой текст с объяснениями.
         self.fail('Закончить тест!')
 
 
-# Она посещает этот URL-адрес – ее список по-прежнему там.
+        # Она посещает этот URL-адрес – ее список по-прежнему там.
 
-# Удовлетворенная, она снова ложится спать
+        # Удовлетворенная, она снова ложится спать
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
