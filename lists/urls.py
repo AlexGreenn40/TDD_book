89ef82d4bf2d5b21517543a82a98e073e7a153b9
AlexGreenn40 <zelalex40@gmail.com>
2023-05-08 23:17:51 +0300

    Настроен исполнитель ФТ для тестирования промежуточного сервера

--- lists/urls.py
+++ lists/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, re_path
 from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     re_path(r'^new$', views.new_list, name='new_list'),
     re_path(r'^(\d+)/$', views.view_list, name='view_list'),
     re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
