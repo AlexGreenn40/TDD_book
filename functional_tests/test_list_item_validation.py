2ca87d12e0788b194426599dc4af352a5a7e59b6
AlexGreenn40 <zelalex40@gmail.com>
2023-05-13 15:36:39 +0300

    Корректировка ФТ

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,6 +7,9 @@ from selenium.webdriver.common.keys import Keys
 ################################################################################
 class ItemValidationTest(FunctionalTest):
     """тест валидации элемента списка"""
+
+    def get_error_element(self):
+        return self.browser.find_element(By.CSS_SELECTOR, '.has-error')
     
     def test_cannot_add_empty_list_items(self):
         """тест: нельзя добавлять пустые элементы списка"""
@@ -53,7 +56,6 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
-
     def test_cannot_add_duplicate_items(self):
         """тест: нельзя добавлять повторяющиеся элементы"""
 
@@ -69,6 +71,29 @@ class ItemValidationTest(FunctionalTest):
 
         # Она видит полезное сообщение об ошибке
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        """тест: сообщения об ошибках очищаются при вводе"""
+        # Эдит начинает список и вызывает ошибку валидации:
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # Она начинает набирать в поле ввода, чтобы очистить ошибку
+        self.get_item_input_box().send_keys('a')
+        
+        # Она довольна от того, что сообщение об ошибке исчезает
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
